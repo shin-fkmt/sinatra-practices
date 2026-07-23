4835e8d1da1019f31bf7b696b2685064f1005c78
shin-fkmt <fkmt.shin@gmail.com>
2026-07-23 09:29:01 +0900

    DDLファイルの取得パス修正

--- db/init_db.rb
+++ db/init_db.rb
@@ -2,11 +2,11 @@
 
 require_relative '../db'
 
-create_db_file_path = File.expand_path('../db/ddl/create_database_memo.sql', __dir__)
+create_db_file_path = File.expand_path('ddl/create_database_memo.sql', __dir__)
 DB.db_name = 'postgres'
 DB.exec(File.read(create_db_file_path), nil)
 DB.clear_db_name
 
-create_table_file_path = File.expand_path('../db/ddl/create_table_memos.sql', __dir__)
+create_table_file_path = File.expand_path('ddl/create_table_memos.sql', __dir__)
 DB.exec(File.read(create_table_file_path), nil)
 DB.close
